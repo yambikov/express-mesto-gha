edf3e01d61ff97fe8c58baff8c701c0f865a3734
yambikov <yambikov@yandex.ru>
2023-10-14 22:45:44 +0300

    07_mongoose installed

--- app.js
+++ app.js
@@ -3,8 +3,15 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use(express.json()); // to support JSON-encoded bodies
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.status(200).send('Привет от GET!');
+});
+
+app.post('/post', (req, res) => {
+  const { name } = req.body;
+  res.status(201).send(`Привет от POST! ${name}`);
 });
 
 app.listen(PORT, () => {
