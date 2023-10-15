05dfcd83759a0a8d65e16cc4d2d703517341faa9
yambikov <yambikov@yandex.ru>
2023-10-15 16:59:59 +0300

    14 CRUD refactoring

--- app.js
+++ app.js
@@ -24,16 +24,15 @@ app.post('/post', (req, res) => {
   res.status(201).send(`Привет от POST! ${name}`);
 });
 
-// CRUD (create, read, update, delete) //
 // GET /users — возвращает всех пользователей
 app.get('/users', (req, res) => {
   UserModel.find()
     .then((data) => {
-      res.status(200).json(data);
+      res.status(200).send(data);
     })
+    // eslint-disable-next-line arrow-body-style, no-unused-vars
     .catch((err) => {
-      console.log(err.message);
-      res.status(500).json({ error: 'Ошибка при получении пользователей' }); // Отправляем ошибку
+      return res.status(500).send({ message: 'Server error' });
     });
 });
 
@@ -41,12 +40,20 @@ app.get('/users', (req, res) => {
 app.get('/users/:userId', (req, res) => {
   const { userId } = req.params;
   UserModel.findById(userId)
+    // eslint-disable-next-line consistent-return
     .then((data) => {
-      res.status(200).json(data);
+      if (!data) {
+        return res.status(404).send({ message: 'User not found' });
+      }
+      res.status(200).send(data);
     })
+    // eslint-disable-next-line consistent-return
     .catch((err) => {
-      console.log(err.message);
-      res.status(500).json({ error: 'Ошибка при получении пользователя' }); // Отправляем ошибку
+      console.log(err);
+      if (err.name === 'CastError') {
+        return res.status(400).send({ message: 'Invalid user ID' });
+      }
+      res.status(500).send({ message: 'Server error' }); // Отправляем ошибку
     });
 });
 
@@ -54,13 +61,18 @@ app.get('/users/:userId', (req, res) => {
 app.post('/users', (req, res) => {
   const { name, about, avatar } = req.body;
 
-  UserModel.create({ name, about, avatar }) // Создаём нового пользователя
+  return UserModel.create({ name, about, avatar }) // Создаём нового пользователя
     .then((data) => {
-      res.status(201).json(data);
+      res.status(201).send(data);
     })
     .catch((err) => {
-      console.log(err.message);
-      res.status(500).json({ error: 'Ошибка при создании пользователя' }); // Отправляем ошибку
+      // console.log(err);
+      // так как ниже мы пишем { message: 'Server error' }, то что действительно произошло
+      // становиться непонятным, поэтому, чтобы дебажить нужно использовать console.log(err)
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({ message: err.message });
+      }
+      return res.status(500).send({ message: 'Server error' });
     });
 });
 
