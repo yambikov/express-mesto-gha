f6b3af3b5643e8a702dfcf325eeae676a0102c5b
yambikov <yambikov@yandex.ru>
2023-10-15 17:48:59 +0300

    16_routes in progress

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require('express');
 // eslint-disable-next-line import/no-extraneous-dependencies
 const mongoose = require('mongoose');
 // const UserModel = require('./models/user');
-const { createUser, getUsers, getUserById } = require('./controllers/users');
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
@@ -16,23 +15,11 @@ const PORT = 3000;
 
 app.use(express.json()); // to support JSON-encoded bodies
 
-app.get('/', (req, res) => {
-  res.status(200).send('Привет от GET!');
-});
+// app.get('/users', getUsers); // get all users
 
-app.post('/post', (req, res) => {
-  const { name } = req.body;
-  res.status(201).send(`Привет от POST! ${name}`);
-});
+// app.get('/users/:userId', getUserById); // get user by id
 
-// GET /users — возвращает всех пользователей
-app.get('/users', getUsers);
-
-// GET /users/:userId - возвращает пользователя по _id
-app.get('/users/:userId', getUserById);
-
-// POST /users — создаёт пользователя
-app.post('/users', createUser);
+// app.post('/users', createUser); // create user
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
---
+++ routes/routes-users.js
@@ -0,0 +1,10 @@
+const userRouter = require('express').Router();
+const { createUser, getUsers, getUserById } = require('../controllers/users');
+
+userRouter.get('/users', getUsers); // get all users
+
+userRouter.get('/users/:userId', getUserById); // get user by id
+
+userRouter.post('/users', createUser); // create user
+
+module.exports = userRouter;
