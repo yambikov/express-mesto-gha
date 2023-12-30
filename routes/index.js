cc345eb31cfc9f50d4f4d74091665b32666dbbf3
yambikov <yambikov@yandex.ru>
2023-12-30 22:49:04 +0300

    123 test

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,15 @@
-// routes/index.js
+// // routes/index.js
 
-const router = require('express').Router();
-// const http2 = require('http2');
-const userRouter = require('./users'); // импортируем роуты юзера
-const cardsRouter = require('./cards'); // импортируем роуты карточек
-// const { ErrorMessages } = require('../utils/errors');
+// const router = require('express').Router();
+// // const http2 = require('http2');
+// const userRouter = require('./users'); // импортируем роуты юзера
+// const cardsRouter = require('./cards'); // импортируем роуты карточек
+// // const { ErrorMessages } = require('../utils/errors');
 
-router.use('/users', userRouter); // подключаем роуты юзера
-router.use('/cards', cardsRouter); // подключаем роуты карточек
+// router.use('/users', userRouter); // подключаем роуты юзера
+// router.use('/cards', cardsRouter); // подключаем роуты карточек
 
-// router.patch('/*', (req, res) => res.status(http2.constants.HTTP_STATUS_NOT_FOUND).send({ message: ErrorMessages.Error404 }));
+// // router.patch('/*', (req, res) =>
+// // res.status(http2.constants.HTTP_STATUS_NOT_FOUND).send({ message: ErrorMessages.Error404 }));
 
-module.exports = router; // экспортируем маршрутизатор
+// module.exports = router; // экспортируем маршрутизатор
